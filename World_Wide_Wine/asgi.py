f4a1cf568d380b557ed4ad81b5cea9ed4841896c
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-02-20 16:08:37 +0000

    initial commit

---
+++ World_Wide_Wine/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for World_Wide_Wine project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'World_Wide_Wine.settings')
+
+application = get_asgi_application()
---
+++ World_Wide_Wine/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for World_Wide_Wine project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'World_Wide_Wine.settings')
+
+application = get_wsgi_application()
