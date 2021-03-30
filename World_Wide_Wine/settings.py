9e85413f6ad22ac9e7ad75d0c7d639f337d75169
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-03-30 17:51:49 +0000

    postgresql datatbase connected

--- World_Wide_Wine/settings.py
+++ World_Wide_Wine/settings.py
@@ -112,8 +112,8 @@ WSGI_APPLICATION = 'World_Wide_Wine.wsgi.application'
 if development:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.path.join(BASE_DIR, 'db.postgresql_psycopg2'),
         }
     }
 else:
