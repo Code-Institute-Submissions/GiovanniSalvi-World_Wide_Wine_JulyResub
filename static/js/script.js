fd5c5294b7100168ddd0e52e6f78b87f7488b9da
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-04-19 21:00:13 +0000

    added login/register template forms

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,18 @@
-$('.nav-link').on('click', function () {
-    if(!$(this).siblings('.nav-link').hasClass('collapsed')) {
-       $(this).siblings('.nav-link').click();
-       }
-    });
+//$('.nav-link').on('click', function () {
+    //if(!$(this).siblings('.nav-link').hasClass('collapsed')) {
+       //$(this).siblings('.nav-link').click();
+       //}
+    //});
+//function list(event, listName) {
+    //var i, text_list, btn;
+    //text_list = document.getElementsByClassName("text_list");
+    //for (i = 0; i < text_list.length; i++) {
+    //text_list[i].style.display = "none";
+    //btn = document.getElementsByClassName("btn");
+    //}
+  //for (i = 0; i < btn.length; i++) {
+    //btn[i].className = btn[i].className.replace(" active", "");
+  //}
+  //document.getElementById(listName).style.display = "block";
+  //event.currentTarget.className += " active";
+//}
