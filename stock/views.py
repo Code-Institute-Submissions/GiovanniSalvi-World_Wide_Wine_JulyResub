6783ca2ddee75ddc8c6ae5b88729bca430aeccfa
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-04-21 18:16:21 +0000

    stock url path added

--- stock/views.py
+++ stock/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+def view_stock(request):
+
+    """ A view that renders the contact form page """
+
+    return render(request, 'stock/stock.html')
