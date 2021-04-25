8d243712d60bc0d68c05bc1aeca956c3927e4a75
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-04-25 23:39:42 +0000

    added items searching and filtering

--- stock/models.py
+++ stock/models.py
@@ -12,15 +12,15 @@ class Stock(models.Model):
 
 class Item(models.Model):
     stock = models.ForeignKey(Stock, null=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=100, null=True, blank=True)
+    sku = models.CharField(max_length=100, blank=True)
     name = models.CharField(max_length=100)
-    types = models.CharField(max_length=100, null=True, blank=True)
+    types = models.CharField(max_length=100, blank=True)
     country = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image_url = models.URLField(null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(blank=True)
+    image = models.ImageField(blank=True)
     objects = models.Manager()
-    
+
     def __str__(self):
         return self.name
--- stock/views.py
+++ stock/views.py
@@ -1,16 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.contrib import messages
 from .models import Item
+
 # Create your views here.
 
 
 def view_stock(request):
 
-    """ A view that renders the contact form page """
-
     item = Item.objects.all()
+    query = None
+    types = None
+    # country = None#
+
+    if request.GET:
+        if 'item' in request.GET:
+            types = request.GET['types']
+            # country = request.GET['country']#
+            item = item.filter(item__types__in=types)
+
+            # item = item.filter(item__country__in=country)#
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "error")
+                return redirect('stock')
+
+            searching = Q(
+                name__icontains=query
+                ) | Q(
+                country__icontains=query) | Q(
+                description__icontains=query
+                )
+
+            item = item.filter(searching)
 
     context = {
         'item': item,
+        'base': query,
+        'stock': types,
     }
 
     return render(request, 'stock/stock.html', context)
