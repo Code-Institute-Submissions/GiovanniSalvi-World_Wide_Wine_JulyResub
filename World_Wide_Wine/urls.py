7a4b468806b38c17da21f10cfadd2a1298be9c18
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-04-30 20:45:49 +0000

    cart app url path added

--- World_Wide_Wine/urls.py
+++ World_Wide_Wine/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('contact/', include('contact.urls')),
     path('stock/', include('stock.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
