9f3b5cef4d8f874eff9d178f6e8a2281a4a99b13
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-05-03 01:54:02 +0000

    cart app url path,views and context added

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from stock.models import Item
 
 
 def cart_contents(request):
@@ -7,6 +9,16 @@ def cart_contents(request):
     cart_list = []
     total = 0
     shopping_counter = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, in cart.items():
+        item = get_object_or_404(Item, pk=item_id)
+        total = item.price
+        shopping_counter = total
+        cart_list.append({
+            'item_id': item_id,
+            'item': item
+        })
 
     if total > settings.FREEDELIVERY_THRESHOLD:
         delivery = 0
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,11 @@ from django.shortcuts import render
 def shopping_cart(request):
 
     return render(request, 'cart/cart.html')
+
+
+def add_shopping(request, item_id):
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
