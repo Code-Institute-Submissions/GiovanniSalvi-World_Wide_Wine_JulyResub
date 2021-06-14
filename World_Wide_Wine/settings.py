ab37d408e360ebf108a1a1219403818bf7802bde
GiovanniSalvi <salvigiovanni4@gmail.com>
2021-06-14 16:01:36 +0000

    debug turned off

--- World_Wide_Wine/settings.py
+++ World_Wide_Wine/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = False
 
 ALLOWED_HOSTS = ['world-wide-wine-django-app.herokuapp.com', 'localhost']
 
